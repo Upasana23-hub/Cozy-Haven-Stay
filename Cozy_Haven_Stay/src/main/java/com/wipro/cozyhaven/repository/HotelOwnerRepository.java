17c8146b9ef11db576126685fcc7bf7dabdb98a5
Khushi Deo <khushideo65@gmail.com>
2026-02-26 15:06:56 +0530

    HotelOwner Repository

--- Cozy_Haven_Stay/src/main/java/com/wipro/cozyhaven/repository/HotelOwnerRepository.java
+++ Cozy_Haven_Stay/src/main/java/com/wipro/cozyhaven/repository/HotelOwnerRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.wipro.cozyhaven.entity.HotelOwner;
 
 public interface HotelOwnerRepository extends JpaRepository<HotelOwner, Long> {
-	Optional<HotelOwner> findByUser_UserId(Long userId);
+
+    Optional<HotelOwner> findByUserId_UserId(Long userId);
 
 }
