a0102e93403c74c4901daa72dad6f1f9e1ddb5d8
Khushi Deo <khushideo65@gmail.com>
2026-02-27 10:57:52 +0530

    It have have Entity as well as sevice layer

--- Cozy_Haven_Stay/src/main/java/com/wipro/cozyhaven/service/HotelServiceImpl.java
+++ Cozy_Haven_Stay/src/main/java/com/wipro/cozyhaven/service/HotelServiceImpl.java
@@ -1,5 +1,91 @@
 package com.wipro.cozyhaven.service;
 
-public class HotelServiceImpl implements IHotelService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.wipro.cozyhaven.entity.Hotel;
+import com.wipro.cozyhaven.entity.HotelOwner;
+import com.wipro.cozyhaven.repository.HotelRepository;
+
+
+@Service
+
+public class HotelServiceImpl implements HotelService {
+
+    private final HotelRepository hotelRepository;
+    
+
+    public HotelServiceImpl(HotelRepository hotelRepository) {
+		super();
+		this.hotelRepository = hotelRepository;
+	}
+
+	@Override
+    public Hotel addHotel(Long ownerId, Hotel hotel) {
+
+        HotelOwner owner = new HotelOwner();
+        owner.setOwnerId(ownerId);
+        hotel.setOwner(owner);
+
+        return hotelRepository.save(hotel);
+    }
+
+    @Override
+    public List<Hotel> getHotelByOwner(Long ownerId) {
+        return hotelRepository.findByOwner_OwnerId(ownerId);
+    }
+
+    @Override
+    public Hotel updateHotel(Long ownerId, Long hotelId, Hotel updatedHotel) {
+
+        Hotel existingHotel = hotelRepository.findById(hotelId)
+                .orElseThrow(() -> new RuntimeException("Hotel not found"));
+
+        if (!existingHotel.getOwner().getOwnerId().equals(ownerId)) {
+            throw new RuntimeException("Unauthorized access");
+        }
+
+        existingHotel.setName(updatedHotel.getName());
+        existingHotel.setLocation(updatedHotel.getLocation());
+        existingHotel.setDescription(updatedHotel.getDescription());
+        existingHotel.setActive(updatedHotel.isActive());
+
+        return hotelRepository.save(existingHotel);
+    }
+
+    @Override
+    public void deleteHotel(Long ownerId, Long hotelId) {
+
+        Hotel hotel = hotelRepository.findById(hotelId)
+                .orElseThrow(() -> new RuntimeException("Hotel not found"));
+
+        if (!hotel.getOwner().getOwnerId().equals(ownerId)) {
+            throw new RuntimeException("Access denied");
+        }
+
+        hotelRepository.delete(hotel);
+    }
+
+	@Override
+	public Hotel getHotelById(Long hotelId) {
+		return hotelRepository.findById(hotelId).orElseThrow(() -> new RuntimeException("Hotel Not Found"));
+	}
+
+	@Override
+	public List<Hotel> getAllActiveHotels() {
+		return hotelRepository.findByActiveTrue();
+	}
+
+	@Override
+	public List<Hotel> searchHotelsByLocation(String location) {
+		return hotelRepository.findByLocationIgnoreCaseAndActiveTrue(location);
+	}
+
+	@Override
+	public List<Hotel> seachHotelsByLocationAndRating(String location, Double minRating) {
+		return hotelRepository.findByLocationIgnoreCaseAndRatingGreaterThanEqualAndActiveTrue(location, minRating);
+	}
+	
+	
 }
