ec7f9ba5420b91c179f14f0e90181bac97ee3ffb
Upasana Chattopadhyay <chattopadhyayupasana2@gmail.com>
2026-02-28 12:15:49 +0530

    validation added

--- Cozy_Haven_Stay/src/main/java/com/wipro/cozyhaven/dto/PaymentDTO.java
+++ Cozy_Haven_Stay/src/main/java/com/wipro/cozyhaven/dto/PaymentDTO.java
@@ -2,6 +2,9 @@ package com.wipro.cozyhaven.dto;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,11 +18,23 @@ import lombok.ToString;
 @ToString
 public class PaymentDTO {
     private Long paymentId;
+    
+    @NotNull(message = "Booking ID is required")
     private Long bookingId;
+    
+    @NotNull(message = "User ID is required")
     private Long userId; 
+    
+    @NotNull(message = "Hotel ID is required")
     private Long hotelId;
+    
+    @NotNull(message = "Payment amount is required")
+    @Positive(message = "Payment amount must be greater than zero")
     private Double amount;
+    
+    @NotBlank(message = "Payment mode is required")
     private String paymentMode;
+    
     private String paymentStatus;
     private LocalDateTime paymentDate;
 }
