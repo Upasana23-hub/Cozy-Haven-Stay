d4ce868f174b960fe7bffbcff340463951862780
Khushi Deo <khushideo65@gmail.com>
2026-03-03 09:58:11 +0530

    I added PreAuthorize in RestController

--- Cozy_Haven_Stay/src/main/java/com/wipro/cozyhaven/restcontroller/HotelOwnerRestController.java
+++ Cozy_Haven_Stay/src/main/java/com/wipro/cozyhaven/restcontroller/HotelOwnerRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,10 +13,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.wipro.cozyhaven.dto.HotelOwnerDTO;
 import com.wipro.cozyhaven.entity.HotelOwner;
 import com.wipro.cozyhaven.entity.User;
 import com.wipro.cozyhaven.service.HotelOwnerService;
+
 import jakarta.validation.Valid;
 
 @RestController
@@ -30,7 +33,7 @@ public class HotelOwnerRestController {
 		this.ownerService = ownerService;
 	}
 
-
+	@PreAuthorize("hasAuthority('ROLE_USER')")
 	@PostMapping("/add")
 	public ResponseEntity<HotelOwner> createOwner(@Valid @RequestBody HotelOwnerDTO dto) {
 		HotelOwner owner = mapToEntity(dto);
@@ -38,36 +41,36 @@ public class HotelOwnerRestController {
 		return new ResponseEntity<>(savedOwner, HttpStatus.CREATED);
 	}
 	
-	
+	@PreAuthorize("hasAuthority('ROLE_OWNER','ROLE_ADMIN')")
 	@GetMapping("/user/{userId}")
 	public ResponseEntity<HotelOwner> getOwnerByUserId(@PathVariable Long userId) {
 		HotelOwner owner = ownerService.getOwnerByUserId(userId);
 		return ResponseEntity.ok(owner);
 	}
 	
-	
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 	@GetMapping
 	public ResponseEntity<List<HotelOwner>> getAllOwners() {
 		return ResponseEntity.ok(ownerService.getAllOwners());
 	}
 	
-	
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 	@PutMapping("/{ownerId}/approve")
     public ResponseEntity<HotelOwner> approveOwner(@PathVariable Long ownerId) {
 		return ResponseEntity.ok(ownerService.approvedOwner(ownerId));
 	
 	}
 	
-	
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 	@PutMapping("/{ownerId}/activate")
     public ResponseEntity<HotelOwner> activateOwner(@PathVariable Long ownerId) {
 		return ResponseEntity.ok(ownerService.activateOwner(ownerId));
     }
 	
-	
-	 @GetMapping("/pending")
-	    public ResponseEntity<List<HotelOwner>> getPendingOwners() {
-	        return ResponseEntity.ok(ownerService.getPendingsOwners());
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+	@GetMapping("/pending")
+	 public ResponseEntity<List<HotelOwner>> getPendingOwners() {
+	     return ResponseEntity.ok(ownerService.getPendingsOwners());
 	 }
 	 
 	 
