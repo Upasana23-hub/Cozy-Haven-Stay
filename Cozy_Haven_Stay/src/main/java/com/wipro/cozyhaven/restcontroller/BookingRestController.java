8b430823d4519f6f0b1504fb95339f8efada0434
A Manjari <manjarianbazhagan@gmail.com>
2026-03-03 10:13:14 +0530

    added Authorization

--- Cozy_Haven_Stay/src/main/java/com/wipro/cozyhaven/restcontroller/BookingRestController.java
+++ Cozy_Haven_Stay/src/main/java/com/wipro/cozyhaven/restcontroller/BookingRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,7 @@ public class BookingRestController {
 	@Autowired
 	BookingService bookingService;
 	
+	@PreAuthorize("hasAnyAuthority('ROLE_USER','ROLE_ADMIN')")
 	@PostMapping("/create")
 	public ResponseEntity<BookingsDTO> createBooking(
                                  @Valid @RequestBody BookingsDTO bookingDTO) 
@@ -34,24 +36,28 @@ public class BookingRestController {
         return new ResponseEntity<>(savedBooking, HttpStatus.CREATED);
     }
 
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 	@GetMapping("/getall")
 	public ResponseEntity<List<BookingsDTO>> getAllBookings() {
         List<BookingsDTO> bookings = bookingService.getAllBookings();
         return new ResponseEntity<>(bookings, HttpStatus.OK);
     }
 	
+	@PreAuthorize("hasAnyAuthority('ROLE_USER','ROLE_OWNER','ROLE_ADMIN')")
 	@GetMapping("/getbyid/{id}")
 	 public ResponseEntity<BookingsDTO> getBookingById(@PathVariable Long bookingId) {
         BookingsDTO booking = bookingService.getBookingById(bookingId);
         return new ResponseEntity<>(booking, HttpStatus.OK);
     }
 	
+	@PreAuthorize("hasAnyAuthority('ROLE_USER','ROLE_ADMIN')")
 	@PutMapping("/cancel/{id}")
     public ResponseEntity<BookingsDTO> cancelBooking(@PathVariable Long bookingId) {
         BookingsDTO cancelledBooking = bookingService.cancelBooking(bookingId);
         return new ResponseEntity<>(cancelledBooking, HttpStatus.OK);
     }
 	
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 	@GetMapping("/payment/{status}")
     public ResponseEntity<List<BookingsDTO>> getBookingByPaymentStatus(
             @PathVariable String paymentStatus) {
