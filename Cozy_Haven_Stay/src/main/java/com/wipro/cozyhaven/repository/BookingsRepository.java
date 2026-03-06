74bfd3983936a19c4883162eb26319d7827561e9
A Manjari <manjarianbazhagan@gmail.com>
2026-03-06 22:11:37 +0530

    done the changes

--- Cozy_Haven_Stay/src/main/java/com/wipro/cozyhaven/repository/BookingsRepository.java
+++ Cozy_Haven_Stay/src/main/java/com/wipro/cozyhaven/repository/BookingsRepository.java
@@ -3,8 +3,9 @@ package com.wipro.cozyhaven.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import com.wipro.cozyhaven.dto.BookingsDTO;
 import com.wipro.cozyhaven.entity.Bookings;
 
 public interface BookingsRepository extends JpaRepository<Bookings, Long> {
@@ -12,5 +13,6 @@ public interface BookingsRepository extends JpaRepository<Bookings, Long> {
 	List<Bookings> findByPaymentStatus(String paymentStatus);
 	
 	List<Bookings> findByUserUserId(Long userId);
-	List<BookingsDTO> getBookingsByHotelId(Long hotelId);
+	@Query("SELECT b FROM Bookings b WHERE b.hotel.hotelId = :hotelId")
+	List<Bookings> getBookingsByHotelId(@Param("hotelId") Long hotelId);
 }
